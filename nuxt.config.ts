016d3d7d5e295213b831010ff548e6ac4dda3c82
kitchanut <kitchanut.r@gmail.com>
2024-12-17 18:53:03 +0700

    Remove Google API

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,9 +33,9 @@ export default defineNuxtConfig({
         }
       ],
       script: [
-        {
-          src: `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&loading=async&libraries=marker`,
-        },
+        // {
+        //   src: `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&loading=async&libraries=marker`,
+        // },
       ],
       meta: [
         {
